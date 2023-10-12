dc4c5f2f597bd2f25fc1893ec3389f4fc25e2da9
KEEMJEE <itscloudy0218@gmail.com>
2023-10-12 09:38:38 +0900

    Commit 1
    Create Project

---
+++ Source/JurassicLand/JurassicLand.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "JurassicLand.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, JurassicLand, "JurassicLand" );
