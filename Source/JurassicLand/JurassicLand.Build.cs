ca6c47bf8e6973d9addbd90045ba60ecd7238f9a
KEEMJEE <itscloudy0218@gmail.com>
2023-10-14 23:05:50 +0900

    Commit 4
    Add TopDownTemplate

--- Source/JurassicLand/JurassicLand.Build.cs
+++ Source/JurassicLand/JurassicLand.Build.cs
@@ -8,7 +8,7 @@ public class JurassicLand : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
---
+++ Source/JurassicLand/Public/CreateObject.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CreateObject.generated.h"
+
+
+UENUM(BlueprintType)
+enum class ECreateObject : uint8 {
+	Tree,
+	Rock,
+	Road,
+	Store,
+};
+
+UCLASS()
+class JURASSICLAND_API ACreateObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ACreateObject();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+};
