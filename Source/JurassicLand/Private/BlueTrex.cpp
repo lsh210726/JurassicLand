06264d4b068de5ce8d976674e34e3429a9b067d2
imagine99 <57169754+imagine99@users.noreply.github.com>
2023-10-25 23:34:32 +0900

    player Rotation

--- Source/JurassicLand/Private/BlueTrex.cpp
+++ Source/JurassicLand/Private/BlueTrex.cpp
@@ -8,6 +8,8 @@
 #include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
 #include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
 #include "../Plugins/EnhancedInput/Source/EnhancedInput/Public/InputMappingContext.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABlueTrex::ABlueTrex()
@@ -25,11 +27,17 @@ ABlueTrex::ABlueTrex()
 		TRexBody->SetRelativeLocation(FVector(0, 0, -87.0f));
 		TRexBody->SetRelativeRotation(FRotator(0, -90.0f, 0));
 	}
+	/*--------- Spring Arm Component Setting -----------*/
+	TRexEyeArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("TRexEyeArm"));
+	TRexEyeArm->SetupAttachment(GetCapsuleComponent());
+	TRexEyeArm->SetRelativeLocation(FVector(0.0f,0.0f,100.0f));
+	TRexEyeArm->SetRelativeRotation(FRotator(-15.0f,0.0f,0.0f));
 
 	/*--------- Camera Component Setting --------*/
 	TRexEye = CreateDefaultSubobject<UCameraComponent>(TEXT("TRexEye"));
-	TRexEye->SetupAttachment(RootComponent);
-	TRexEye->SetRelativeLocation(FVector(290.0f, 0.0f, 170.0f));
+	TRexEye->SetupAttachment(TRexEyeArm);
+// 	TRexEye->SetWorldLocation(FVector(0.0f,0.0f,0.0f));
+// 	TRexEye->SetWorldRotation(FRotator(0.0f,0.0f,0.0f));
 
 	/*--------- Enhanced Input Setting ----------*/
 	static ConstructorHelpers::FObjectFinder<UInputMappingContext>tempIMC_TRex(TEXT("/Game/7_MISC/Input/IMC_TRex.IMC_TRex"));
@@ -39,6 +47,8 @@ ABlueTrex::ABlueTrex()
 	}
 
 	
+// 	AttachToComponent(TRexEye,FAttachmentTransformRules::KeepRelativeTransform,TRexBody->GetSocketBoneName("bn_Head_10"));
+// }
 }
 
 // Called when the game starts or when spawned
@@ -76,6 +86,7 @@ void ABlueTrex::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void ABlueTrex::SetupPlayerEnhancedInputComponent(class UEnhancedInputComponent* EnhancedInputComponent, TArray<class UInputAction*> InputActions)
 {
 	EnhancedInputComponent->BindAction(InputActions[0], ETriggerEvent::Triggered, this, &ABlueTrex::TRexMove);
+	EnhancedInputComponent->BindAction(InputActions[1],ETriggerEvent::Triggered,this,&ABlueTrex::TRexLook);
 }
 
 void ABlueTrex::TRexMove(const FInputActionValue& Val)
@@ -87,5 +98,18 @@ void ABlueTrex::TRexMove(const FInputActionValue& Val)
 	FRotator CurrControlRotation = GetControlRotation();
 	AddMovementInput(UKismetMathLibrary::GetRightVector(FRotator(0.0f, CurrControlRotation.Yaw, CurrControlRotation.Roll)), valX);
 	AddMovementInput(UKismetMathLibrary::GetForwardVector(FRotator(0.0f, CurrControlRotation.Yaw, 0.0f)), valY);
+	
+
+}
+
+void ABlueTrex::TRexLook(const FInputActionValue& Val)
+{
+	FVector2D tempVal = Val.Get<FVector2D>();
+	float valX = tempVal.X;
+	float valY = tempVal.Y;
+
+	AddControllerYawInput(valX);
+	AddControllerPitchInput(valY);
+
 }
 
